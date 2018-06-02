df032e7b5711e6cc2f5c7fbdb07e91cf4c3f0e7f
Jack Li <liyunzhejack@gmail.com>
2018-06-02 16:11:35 -0700

    function define

--- MetropolisHastings.R
+++ MetropolisHastings.R
@@ -9,6 +9,28 @@
 # likelihood distribution: poisson distirbution, lambda=1
 # prior distributions: slope: gamma(3,7), intercept: unif, sd: exponential(5)
 # proposal distributions: Normal((1,2,3), (1,4,9))
-#
+
+likelihood_func <- function(){
+# get likelihood function
+}
+
+prior_func <- function(){
+# prior 
+}
+
+prosterior_func <- function(){
+# posterior
+}
+
+proposal_func <- function(){
+# proposal function
+}
+
+metropolis_Hastings_func <- function(){
+# actual function
+}
+
+## real parameters
+
 
 

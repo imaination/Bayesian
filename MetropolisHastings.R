67804d7b3ecf2b4fc5f80beb9b23a2f43b95753b
Jack Li <liyunzhejack@gmail.com>
2018-06-02 16:00:18 -0700

    set-up

---
+++ MetropolisHastings.R
@@ -0,0 +1,14 @@
+### June 2.
+### Authors: Ashly, Bella, Jack
+### MetropolisHastrings.R
+#
+# This script is simulating numbers for a linear regression model.
+# We have three parameters, slopt, intercept, and standard deviation
+# of error. 
+# 
+# likelihood distribution: poisson distirbution, lambda=1
+# prior distributions: slope: gamma(3,7), intercept: unif, sd: exponential(5)
+# proposal distributions: Normal((1,2,3), (1,4,9))
+#
+
+

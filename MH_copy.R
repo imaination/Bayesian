289f8c3692e0ff48ab227ad7edd06ed241b53796
yizi <yizzhang@ucdavis.edu>
2018-06-03 19:50:42 -0700

    haha

--- MH_copy.R
+++ MH_copy.R
@@ -11,26 +11,20 @@
 # prior distributions: slope: gamma(3,7), intercept: unif, sd: exponential(5)
 # proposal distributions: Normal((1,2,3), (1,4,9))
 
-# real parameters
-trueSlope = 5
-trueIntercept = 2
-trueSd = 10
-smapleSize = 422
+library("coda")
 
-# range of x
-x = (-(sampleSize-1)/2):((sampleSize-1)/2) 
-# linear regression model w/ normally distributed error
-y = trueSlope * x + trueIntercept + rnorm(n=sampleSize, mean=0, sd=trueSD)
-
-likelihood_func <- function(param){
+likelihood_func <- function(param, x, y){
 # get likelihood function
 	slope = param[1]
 	intercept = param[2]
-	sd = paramp[3]
+	sd = param[3]
 	y_hat = slope*x + intercept
-	singlelikelihood = dpois(y, lamda = 1)
-	nlikelihood = prod(singlelikelihood)
-	return nlikelihood
+	# print(y_hat)
+	singlelikelihood = dnorm(y, mean=y_hat, sd=sd, log=T)
+	# print(singlelikelihood)
+	nlikelihood = sum(singlelikelihood)
+	#print(nlikelihood)
+	nlikelihood
 }
 
 prior_func <- function(param){
@@ -38,16 +32,18 @@ prior_func <- function(param){
 	slope = param[1]
 	intercept = param[2]
 	sd = param[3]
-	slope_prior = dgamma(slope, shape=3, rate=7)
-	intercept_prior = dunif(intercept, min=0, max=10)
-	sd_prior = dexp(sd, rate=5)
-	return(slope_prior*intercept_prior*sd_prior)
+	slope_prior = dgamma(slope, shape=3, rate=7, log=T)
+	intercept_prior = dunif(intercept, min=0, max=10, log=T)
+	sd_prior = dexp(sd, rate=5, log=T)
+	return(slope_prior+intercept_prior+sd_prior)
 	
 }
 
-prosterior_func <- function(param){
+posterior_func <- function(param, x, y){
 # posterior
-	return(likelihood_func(param)*prior_func(param))
+  l = likelihood_func(param, x, y)
+  p = prior_func(param)
+	return(l + p)
 }
 
 proposal_func <- function(param){
@@ -55,13 +51,13 @@ proposal_func <- function(param){
 	return(rnorm(3, mean=param, sd = c(0.1, 0.4, 0.9)))
 }
 
-metropolis_Hastings_func <- function(start_value, iter){
+metropolis_Hastings_func <- function(start_value, iter, x, y){
 # actual function
-	chain = matrix(c(), nrow=iter+1, ncol=3)
+	chain = array(dim = c(iter+1,3))
 	chain[1,] = start_value
 	for (i in 1:iter){
 		proposal = proposal_func(chain[i,])
-		prob = posterior_func(proposal) / posterior(chain[i,])
+		prob = exp(posterior_func(proposal, x, y) - posterior_func(chain[i,], x, y))
 		if (runif(1) < prob){
 			chain[i+1,] = proposal
 		}
@@ -69,27 +65,28 @@ metropolis_Hastings_func <- function(start_value, iter){
 			chain[i+1,] = chain[i,]
 		}
 	}
-	return(mcmc(chain))
+	result = mcmc(chain)
+	return(result)
 }
 
 # real parameters
 trueSlope = 5
 trueIntercept = 2
 trueSd = 10
-smapleSize = 422
+sampleSize = 422
 
 # range of x
 x = (-(sampleSize-1)/2):((sampleSize-1)/2) 
 # linear regression model w/ normally distributed error
-y = trueSlope * x + trueIntercept + rnorm(n=sampleSize, mean=0, sd=trueSD)
+y = trueSlope * x + trueIntercept + rnorm(n=sampleSize, mean=0, sd=trueSd)
 
 # set initial point
 start_value = c(3, 2, 1)
-chain = metropolis_Hastings_func(start_value, 20000)
+chain = metropolis_Hastings_func(start_value, 20000, x, y)
 
 burnIn = 10000
 acceptRatio = 1-mean(duplicated(chain[-(1:burnIn),]))    ### REDEFINE ?
 
 # analysis
 summary(chain)
-plot(chain)
+plot(mcmc(chain))

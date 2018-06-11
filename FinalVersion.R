448626050d0558e52146ecf8e0d1a7730850ec42
yizi <yizzhang@ucdavis.edu>
2018-06-10 19:01:59 -0700

    yeah

--- FinalVersion.R
+++ FinalVersion.R
@@ -4,15 +4,15 @@ library(BayesianTools)
 set.seed(54321)
 
 true_theta<-c(0.1,0.7,0.2)
-param<-c(2,2,2)
+alpha<-c(2,2,2)
 x<-rmultinom(20,size=7,prob = true_theta)   # FIX THIS
 
 gibbs_func<-function(start_value,burnin,thin,iter){
   update<-start_value
   theta_mat<-mat.or.vec(2,iter)
   for(i in 1:iter){
-    theta1<-(1-update[2])*(rbeta(1,shape1 = param[1]+sum(x[1,]),shape2 = param[3]+sum(x[3,])))
-    theta2<-(1-theta1)*(rbeta(1,shape1 = param[2]+sum(x[2,]),shape2 = param[3]+sum(x[3,])))
+    theta1<-(1-update[2])*(rbeta(1,shape1 = alpha[1]+sum(x[1,]),shape2 = alpha[3]+sum(x[3,])))
+    theta2<-(1-theta1)*(rbeta(1,shape1 = alpha[2]+sum(x[2,]),shape2 = alpha[3]+sum(x[3,])))
     update<-c(theta1,theta2)
     theta_mat[,i]<-update
   }
@@ -90,7 +90,7 @@ plot(mcmc(metro_chain))
 # 
 # # check convergence
 # chain2<-gibbs(c(sum(x[1,])/140,sum(x[2,])/140),1000,100,1000000)
-# combinedchains = mcmc.list(mcmc(chain), mcmc(chain2))
-# plot(combinedchains)
-# gelman.diag(combinedchains)
-# gelman.plot(combinedchains)
+combinedchains = mcmc.list(mcmc(gibbs_chain), mcmc(metro_chain))
+plot(combinedchains)
+gelman.diag(combinedchains)
+gelman.plot(combinedchains)
